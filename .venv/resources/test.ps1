935e39c5da52d218c748efbed4ae45246017e8e3
Matteus Maximo <matteusmaximof@gmail.com>
2023-07-17 09:25:27 -0300

    base-project

--- .venv/resources/test.ps1
+++
@@ -1,3 +0,0 @@
-if (hostname -match 'CPD'){
-    
-}
--- .venv/src/database/hosts.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Optional
-from sqlalchemy import ForeignKey, String, Integer
-from sqlalchemy.orm import Base, Mapped, relationship, mapped_column
-
-class Host(Base):
-    __tablename__ = "Host"
-    
-    hostname : Mapped[String] = mapped_column(String(120), primary_key=True)
-    port : Mapped[Integer] = mapped_column(Integer)

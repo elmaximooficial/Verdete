c5526f8e7ddfec977ccb7ae0d246289d1d350634
Matteus Maximo <matteusmaximof@gmail.com>
2023-07-21 13:17:17 -0300

    Some async stuff

--- src/database/db_handler.py
+++ src/database/db_handler.py
@@ -60,6 +60,7 @@ class DBHandler:
         collection.insert_one(value)
     
     def upsert(self, collection, value: dict):
+        print(value)
         collection = self.__connection.verdete.get_collection(collection)
         collection.update_one({"Hostname": value["Hostname"]}, {"$set": {value["Task Name"]: value}}, upsert=True)
     

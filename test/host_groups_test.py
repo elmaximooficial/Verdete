afbc46218cbd854787d05f0277dc89eb7069c08e
Matteus Maximo <matteusmaximof@gmail.com>
2023-07-26 10:06:30 -0300

    Added some unit testing

--- test/host_groups_test.py
+++ test/host_groups_test.py
@@ -15,13 +15,12 @@ async def main():
     await ldap_conn.connect_to_server()
 
     print("Creating Host Group in Test")
-    test_group = await HostGroup.fetch_hostgroup(
-        hosts=[Host(i) async for i in ldap_conn.fetch_computers()],
-        name="All",
-        description="All Hosts",
-        user=User("PA\Administrador", "¨TF6yh7uj"))
+    test_group = await HostGroup.fetch_hostgroup(name="All")
+    cpd_group = await HostGroup.create_hostgroup(name="CPD")
+    async for i in cpd_group:
+        print(i)
     print("Created Host Group")
-    task_group = await WinRMTaskGroup.fetch_task(tasks=[WinRMTask("Hostname", '"`nhostname`n$(hostname)"')], name="Hostname")
+    task_group = await WinRMTaskGroup.fetch_task(name="Hostname")
     await task_group.execute(group=test_group, debug=False)
 
 if __name__ == "__main__":
